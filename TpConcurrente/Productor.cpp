58d6ca4291df3bfebd6f9d481ac920f424f6b597
gabriel piselli <152130234+GabrielPiselli1@users.noreply.github.com>
2026-06-06 19:11:21 -0300

    Refactor productor functions for clarity and consistency

--- TpConcurrente/Productor.cpp
+++ TpConcurrente/Productor.cpp
@@ -1,69 +1,47 @@
 #include "Productor.h"
-
 #include <thread>
 #include <cstdlib>
 #include <chrono>
 
-void productor(SistemaLogistico& sistema,int cantidad)
-{
-    for(int i = 0; i < cantidad; i++)
-    {
+void productor(SistemaLogistico& sistema,int cantidad){
+    for(int i = 0; i < cantidad; i++){
         Paquete p;
-
         p.tiempoCreacion = obtenerTiempoMs();
-
+        
         p.prioridad = rand() % 2;
-
-        sistema.mutexID.wait();
-
-        sistema.generadorID++;
-
-        p.id = sistema.generadorID;
-
-        sistema.paquetesProducidos++;
-
+            sistema.mutexID.wait();
+            sistema.generadorID++;
+            p.id = sistema.generadorID;
+            sistema.paquetesProducidos++;
         sistema.mutexID.signal();
 
         std::this_thread::sleep_for(std::chrono::milliseconds(90));
 
         sistema.mutexColas.wait();
-
-        sistema.waitingQueue.push_back(p);
-
+            sistema.waitingQueue.push_back(p);
         sistema.mutexColas.signal();
 
         sistema.sincronizacion.signal();
     }
 }
 
-void productorPrioridadFija(SistemaLogistico& sistema,int cantidad,int prioridad)
-{
+void productorPrioridadFija(SistemaLogistico& sistema,int cantidad,int prioridad){
     for(int i = 0; i < cantidad; i++)
     {
         Paquete p;
-
         p.tiempoCreacion = obtenerTiempoMs();
-
         p.prioridad = prioridad;
-
         sistema.mutexID.wait();
-
-        sistema.generadorID++;
-
-        p.id = sistema.generadorID;
-
-        sistema.paquetesProducidos++;
-
+            sistema.generadorID++;
+            p.id = sistema.generadorID;
+            sistema.paquetesProducidos++;
         sistema.mutexID.signal();
 
         std::this_thread::sleep_for(std::chrono::milliseconds(90));
 
         sistema.mutexColas.wait();
-
-        sistema.waitingQueue.push_back(p);
-
-        sistema.mutexColas.signal();
-
+            sistema.waitingQueue.push_back(p);
+            sistema.mutexColas.signal();
         sistema.sincronizacion.signal();
     }
 }
